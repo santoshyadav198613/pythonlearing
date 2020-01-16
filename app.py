a7065886b0b5125bc92bd25a0605e7e499d048c2
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-01-17 01:42:46 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+print('Santosh Yadav')
+print('*' * 10)
+
+# name = 'John Smith'
+# age = 20
+# is_new = True
+
+# name = input("What is your name? ")
+# favorite_color = input("What is your favorite color? ")
+# print(name + ' likes ' + favorite_color)
+
+# weight_pounds = input("Enter your weight in pounds ")
+# weight_kg = int(weight_pounds) * 0.45
+# print(weight_kg)
+
+name = 'Jennifer'
+print(name[1:-1])
